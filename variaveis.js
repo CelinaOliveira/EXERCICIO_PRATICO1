cd09ab6464171503421dc87c87060eac9184c8ba
Celina Oliveira <celinaoliveira98@gmail.com>
2022-05-31 17:14:33 -0300

    subindo atividade 1

---
+++ variaveis.js
@@ -0,0 +1,7 @@
+let meuNome = "Celina";
+let idade = 24;
+let populacao = 214.388;
+let verdadeiro = true;
+let falso = false
+
+console.log("Meu nome e " + meuNome +" e tenho " + idade + " anos." + " No Brasil ha mais de " + populacao + " pessoas. Voce tambem é brasileiro:" + verdadeiro);
